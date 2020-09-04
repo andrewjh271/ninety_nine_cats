2d46dc4eac4f26bcaccc893e4ea9953673f45b67
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-09-04 16:21:05 -0700

    Added rental request views

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,6 +6,7 @@ class CatsController < ApplicationController
 
   def show
     @cat = Cat.find_by(id: params[:id])
+    @rental_requests = CatRentalRequest.where(cat_id: @cat.id).order(:start_date)
     render :show
   end
 
