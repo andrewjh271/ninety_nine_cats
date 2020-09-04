08bf82527fbb36a6c45ad379066248c58d3183fa
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-09-03 17:15:21 -0700

    Added Cat migration and model with validations

---
+++ app/models/cat.rb
@@ -0,0 +1,35 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birthdate   :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  COLORS = %w[black white tuxedo calico orange tortoiseshell]
+
+  validates :birthdate, :color, :name, :sex, :description, presence: true
+  validates :color, inclusion: { in: COLORS }
+  validates :sex, length: { is: 1 }
+  validate :no_future_birthdate
+
+  def age
+    time_ago_in_words(birthdate)
+  end
+
+  private
+
+  def no_future_birthdate
+    if birthdate > Date.today
+      errors[:birthdate] << 'Cat\'s birthdate can\'t be in the future'
+    end
+  end
+end
