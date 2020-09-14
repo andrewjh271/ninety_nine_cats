fd9d6eec552d4fece2595872cf792ae1ff371b3e
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-09-13 19:58:41 -0700

    Added route for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'cats#index'
+
   resources :cats
   resources :cat_rental_requests, only: [:create, :new, :show] do
     member do
